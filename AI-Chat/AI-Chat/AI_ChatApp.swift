259aa89bf0bafeadd8e8f0e418f8114527276807
Desire Namanya <100676695+DesireNamanya@users.noreply.github.com>
2023-01-27 13:02:19 +0300

    First Commit

---
+++ AI-Chat/AI-Chat/AI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatApp.swift
+//  AI-Chat
+//
+//  Created by Desire namanya on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-Chat/AI-Chat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AI-Chat
+//
+//  Created by Peter Kure on 19.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Peter")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
